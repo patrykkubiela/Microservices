e2fd4ccf3f17d77f60f65e61c5bff73c8d76e21c
Patryk Kubiela <patryk.kubiela@programista-doswiadczony.pl>
2021-10-29 12:58:22 +0200

    rename controller to platforms controller

---
+++ ComandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ComandsService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly ILogger<PlatformsController> _logger;
+
+        public PlatformsController(ILogger<PlatformsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new[] {"empty"};
+        }
+    }
+}
